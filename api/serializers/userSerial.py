61781936a4664d7ddc2df9e3e43bf9dd4e9b868a
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-10-07 10:27:06 +0530

    some changes

--- api/serializers/userSerial.py
+++ api/serializers/userSerial.py
@@ -1,22 +1,7 @@
 from rest_framework import serializers
-from api.models.user import User
-class userSerializer(serializers.ModelSerializer):
+from api.models.user import *
 
-    user_id = serializers.PrimaryKeyRelatedField(read_only = True)
-    full_name = serializers.ReadOnlyField()
-    display_picture = serializers.ReadOnlyField()
-    enrolment_number = serializers.ReadOnlyField()
-    online_status = serializers.ReadOnlyField()
-    email = serializers.EmailField(read_only = True)
+class userSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = [
-            'user_id',
-            'full_name', 
-            'display_picture', 
-            'enrolment_number', 
-            'user_type',
-            'online_status',
-            'is_disabled',
-            'email',
-        ]
+        fields = '__all__'
