4c8b4edbe118a2aa903e81d9e45ee7cb622370d1
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-10-18 23:03:38 +0530

    websocket implemented

---
+++ api/Channels/routing.py
@@ -0,0 +1,8 @@
+# routing.py
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/some_path/$', consumers.ProjectConsumer.as_asgi()),
+]
--- backend/asgi.py
+++ backend/asgi.py
@@ -1,16 +1,13 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
+# asgi.py
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from api.Channels.routing import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(routing.websocket_urlpatterns),
+})
