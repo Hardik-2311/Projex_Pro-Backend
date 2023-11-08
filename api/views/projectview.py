665511a476b766f5df02d5109bc7640ac7d9a1a4
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-08 18:34:44 +0530

    projet view done

--- api/views/projectview.py
+++ api/views/projectview.py
@@ -1,6 +1,6 @@
-from rest_framework import generics,viewsets
-from api.serializers import *
-from api.models import *
+# from rest_framework import generics,viewsets
+# from api.serializers import *
+# from api.models import *
 
 
 # class ProjectViewSet(viewsets.ModelViewSet):
@@ -41,6 +41,42 @@ from api.models import *
 #         instance.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
 
+# class ProjectViewSet(viewsets.ModelViewSet):
+#      queryset = project.objects.all()
+#      serializer_class = ProjectSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from api.permissions import IsSessionAuthenticated
+from api.models.project import project  # Import your Project model
+from api.serializers.projectSerializer import ProjectSerializer  # Import your Project serializer
+
 class ProjectViewSet(viewsets.ModelViewSet):
-     queryset = project.objects.all()
-     serializer_class = ProjectSerializer
+    queryset = project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsSessionAuthenticated]
+    lookup_url_kwarg = "id"
+    http_method_names = ["get", "put", "patch", "delete", "post"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, *args, **kwargs):
+        project = self.get_object()
+        serializer = self.get_serializer(project)
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        project = self.get_object()
+        serializer = self.get_serializer(project, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        project = self.get_object()
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
