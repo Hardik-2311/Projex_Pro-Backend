5ec08f446c183d066878e9bc8dc4b60a467324ec
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-08 19:04:51 +0530

    task view sets

--- api/views/taskview.py
+++ api/views/taskview.py
@@ -1,34 +1,44 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 from api.models.task import task
 from api.serializers.Taskserializer import taskSerializer
+from api.permissions import IsSessionAuthenticated
+from django.shortcuts import get_object_or_404
 
-
-# class TaskCreateOrListViewSet(generics.ListCreateAPIView):
-#     serializer_class = taskSerializer
-#     permission_classes = [IsAuthenticated, IsAdminUser]
-
-#     def get_queryset(self):
-#         return task.objects.all()
-
-#     def perform_create(self, serializer):
-#         serializer.save(creator=self.request.user)
-
-# class TaskDetailViewSet(generics.RetrieveUpdateDestroyAPIView):
-#     serializer_class = taskSerializer
-#     permission_classes = [IsAuthenticated, IsAdminUser]
-#     lookup_url_kwarg = 'task_id'
-#     http_method_names = ['get', 'post', 'head', 'patch', 'delete']
-
-#     def get_queryset(self):
-#         return task.objects.all()
-
-#     def retrieve(self, request, *args, **kwargs):
-#         queryset = task.objects.all()
-#         task_instance = generics.get_object_or_404(queryset, pk=self.kwargs['task_id'])
-#         serializer = taskSerializer(task_instance)
-#         res_dict = serializer.data
-#         res_dict['creator'] = userSerializer(task_instance.creator).data
-#         return Response(res_dict, status=status.HTTP_200_OK)
 class TaskDetailViewSet(viewsets.ModelViewSet):
     queryset = task.objects.all()
     serializer_class = taskSerializer
+    permission_classes = [IsSessionAuthenticated]
+    lookup_url_kwarg = "id"
+    http_method_names = ["get", "put", "patch", "delete", "post"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, *args, **kwargs):
+        task = self.get_object()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        task = self.get_object()
+        serializer = self.get_serializer(task, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        task = self.get_object()
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
